1b4600f1aff402fdcd021d60d2654d070b95a725
Vanessa <vanessabia2112@gmail.com>
2026-05-31 22:08:11 -0300

    feat(auth): implementa fluxo completo de autenticação

--- financas-webapp/src/pages/Register/Register.tsx
+++ financas-webapp/src/pages/Register/Register.tsx
@@ -1,18 +1,42 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import type { AppDispatch, RootState } from "../../app/store";
+import { register } from "../../feature/auth/authThunks";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
   const [name, setName] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+
+  const error = useSelector(
+    (state: RootState) => state.auth.error
+  );
+
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    console.log({
-      name,
-      username,
-      password,
-    });
+    if (!name || !username || !password) {
+      alert("Preencha todos os campos");
+      return;
+    }
+
+    try {
+      await dispatch(
+        register({
+          name,
+          username,
+          password,
+        })
+      ).unwrap();
+
+      navigate("/login");
+    } catch (error) {
+      console.error("Erro ao cadastrar usuário", error);
+    }
   };
 
   return (
@@ -48,6 +72,13 @@ function Register() {
         </div>
 
         <button type="submit">Cadastrar</button>
+
+        {error && (
+          <p style={{ color: "red" }}>
+            {error}
+          </p>
+        )}
+
       </form>
     </div>
   );
